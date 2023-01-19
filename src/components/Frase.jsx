ccb96d5f2562cd1f74800ad6eeb0ec2604e708a6
Nicolas Gomez Marigliano <rodnicgm@gmail.com>
2023-01-19 09:14:37 -0300

    maquetado inicial

---
+++ src/components/Frase.jsx
@@ -0,0 +1,21 @@
+const Frase = () => {
+    return (
+        <section className="row bg-white my-5 p-5 m-1">
+            <aside className="col-4 m-5 text-center">
+            <img
+              src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd857ffabe0042d12baf2/download/image.png"
+              alt="the simpsons"
+              className="w-100"
+            />
+           </aside>
+
+
+
+        </section>
+    )
+
+
+
+
+
+}
